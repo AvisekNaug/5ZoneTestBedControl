e354afee9f5398ecaf6320ea5d15c0200381d45b
Avisek <aviseknaug1993@gmail.com>
2021-05-06 13:01:52 -0500

    updates

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='5zone',
+    name='zone5',
     version='1.0.0',
-    packages=find_packages(include=['5zone']),
+    packages=find_packages(include=['zone5']),
     description='Use Testbed as a standalone installation',
     author='Avisek Naug',
     author_email='avisekvandy@gmail.com',
